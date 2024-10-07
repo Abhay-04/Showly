e0f7a9249f99832f3d77724cac8bf5ad63c0a030
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2024-10-07 19:30:17 +0530

    UX : hide dropdown when click outside

--- src/components/HeaderBrowse.jsx
+++ src/components/HeaderBrowse.jsx
@@ -3,13 +3,14 @@ import Logo from "../../src/file.png";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { addUser, removeUser } from "../store/userSlice";
 import { USER_AVATAR } from "../utils/constants";
 import { Link, useNavigate } from "react-router-dom";
 
 const HeaderBrowse = () => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -43,7 +44,25 @@ const HeaderBrowse = () => {
 
   const handleToggleDropdown = () => {
     setShowDropdown(!showDropdown);
+    
   };
+
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setShowDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    // Add event listener to detect clicks outside the dropdown
+    document.addEventListener('mousedown', handleClickOutside);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   const user = useSelector((store) => store.user);
 
@@ -62,7 +81,7 @@ const HeaderBrowse = () => {
       <div className="logo">
         <img className=" w-28 lg:w-36 h-auto  " src={Logo} alt="Logo" />
       </div>
-      <div className="right-nav flex items-center gap-2 relative">
+      <div onClick={handleToggleDropdown} className="right-nav flex items-center gap-2 relative cursor-pointer">
         <div className="flex items-center">
           <img
             className="w-8 h-8 rounded-full mx-2"
@@ -73,14 +92,14 @@ const HeaderBrowse = () => {
             {user?.displayName ? user?.displayName : "Guest"}
           </p>
           <i
-            className="ri-arrow-down-s-fill cursor-pointer"
-            onClick={handleToggleDropdown}
+            className="ri-arrow-down-s-fill "
+            
           ></i>
         </div>
 
         {/* Dropdown menu */}
         {showDropdown && (
-          <div className="absolute -bottom-48 right-0 mt-2 bg-transparent text-white font-semibold  rounded-md shadow-lg">
+          <div ref={dropdownRef} className="absolute -bottom-48 right-0 mt-2 bg-transparent text-white font-semibold  rounded-md shadow-lg">
             <ul className="list-none bg-[#181E24] px-4 py-2 cursor-pointer rounded-lg  w-40 h-auto  ">
               <li className="my-2 hover:bg-gray-300 rounded-md px-2 py-1">
                 <i className="ri-user-3-fill mr-4"></i>
