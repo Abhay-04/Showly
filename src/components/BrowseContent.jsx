c9b71fead4e489d719f77e1643492ad5d72ee0de
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2024-10-19 23:31:48 +0530

    add: yt trailer with details

--- src/components/BrowseContent.jsx
+++ src/components/BrowseContent.jsx
@@ -15,6 +15,9 @@ const BrowseContent = () => {
   const dispatch = useDispatch();
 
   const trailerKey = useSelector((store) => store.movies.randomMovieKey);
+  const randomNowPlayingMovie = useSelector(
+    (store) => store.movies.randomNowPlayingMovie
+  );
 
   const backdrop_path = useSelector(
     (store) => store?.movies?.randomNowPlayingMovie?.backdrop_path
@@ -49,7 +52,20 @@ const BrowseContent = () => {
 
   return (
     trailerKey && (
-      <VideoTrailer trailerKey={trailerKey} />
+      <div className="relative h-[55vh] w-full bg-gradient-to-r from-black">
+        <VideoTrailer trailerKey={trailerKey} />
+        <div className="h-full flex flex-col justify-end  text-white  px-16 pb-12 w-[50vw]">
+          <h1 className="text-3xl font-bold">{randomNowPlayingMovie.title}</h1>
+          <p className="text-lg ">{randomNowPlayingMovie.overview}</p>
+          <div className="flex gap-4">
+            <span>
+              <i className="ri-megaphone-fill mr-2 text-yellow-500"></i>
+              {randomNowPlayingMovie.release_date}
+            </span>
+            <span><i className="ri-movie-2-fill mr-2 text-yellow-500"></i> Movie</span>
+          </div>
+        </div>
+      </div>
     )
   );
 };
--- src/components/VideoTrailer.jsx
+++ src/components/VideoTrailer.jsx
@@ -1,18 +1,21 @@
+import ReactPlayer from "react-player";
+
 const VideoTrailer = ({ trailerKey }) => {
   return (
-    <div className="w-[100%] h-[45vh]">
-      <iframe
-        width="100%"
-        height="100%"
-        className="object-cover bg-center bg-gray-600 h-[100%]"
-        src={`https://www.youtube.com/embed/${trailerKey}?si=701bB7fdbgG29UHX?autoplay=1&controls=0&modestbranding=1&showinfo=0&rel=0&mute=1`}
-        title="YouTube video"
-        allowFullScreen
-      ></iframe>
+    <div className="w-full h-full -z-10  absolute ">
+      {/* <iframe
+          className="w-full h-full object-cover absolute  "
+          src={`https://www.youtube.com/embed/${trailerKey}?autoplay=1&controls=0&modestbranding=1&showinfo=0&rel=0&mute=1`}
+          title="YouTube video"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        ></iframe> */}
+      <ReactPlayer
+        url={`https://www.youtube.com/embed/${trailerKey}?autoplay=1&controls=0&modestbranding=1&showinfo=0&rel=0&mute=1`}
+        playing={true} muted={true} width={"100%"} height={"100%"} loop={true}
+      />
     </div>
   );
 };
 
 export default VideoTrailer;
-
-// https://www.youtube.com/embed/ZdyaixHYviA?si=FnrTjF-iRfDhurJi?autoplay=1&mute=1
