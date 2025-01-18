34e721107921d81b89e7a27689b2b95c5b7115f1
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2025-01-18 16:21:26 +0530

    add : styling

--- src/components/BottomBrowse.jsx
+++ src/components/BottomBrowse.jsx
@@ -1,41 +1,62 @@
 ;
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import lang from "../utils/languageConstants";
 
 const BottomBrowse = () => {
 
   const langKey = useSelector((store) => store.config.language);
+
+ const location  = useLocation()
+  
   return (
-    <div className="bg-[#181E24] grid grid-cols-5 w-full lg:hidden h-auto fixed z-[10000] py-3  bottom-0">
+    <div className="bg-[#181E24] grid grid-cols-6 w-full lg:hidden h-auto fixed z-[10000] py-3  bottom-0">
       
+        <Link to={"/browse"}>
+          <div className={`text-white text-xs font-bold flex flex-col items-center justify-center rounded-lg ${
+          location.pathname === "/browse" ? "bg-red-500" : ""
+        }`}>
+          <i className="ri-home-line"></i>
+          {"Home"}
+          </div>
+        </Link>
         <Link to={"/browse/trending"}>
-          <div className="text-white text-xs font-bold flex flex-col items-center justify-center">
+          <div className={`text-white text-xs font-bold flex flex-col items-center justify-center rounded-lg ${
+          location.pathname === "/browse/trending" ? "bg-red-500" : ""
+        }`}>
           <i className="ri-fire-line "></i>
           {lang[langKey].trending}
           </div>
         </Link>
         <Link to={"/browse/popular"}>
-          <div className="text-white text-xs font-bold flex flex-col items-center justify-center">
+          <div className={`text-white text-xs font-bold flex flex-col items-center justify-center rounded-lg ${
+          location.pathname === "/browse/popular" ? "bg-red-500" : ""
+        }`}>
             <i className="ri-bard-line "></i>
             {lang[langKey].popular}
           </div>
         </Link>
         <Link to={"/browse/movies"}>
-          <div className="text-white text-xs font-bold flex flex-col items-center justify-center">
+          <div className={`text-white text-xs font-bold flex flex-col items-center justify-center rounded-lg ${
+          location.pathname === "/browse/movies" ? "bg-red-500" : ""
+        }`}>
             <i className="ri-movie-2-line"></i>
             {lang[langKey].movies}
           </div>
         </Link>
         <Link to={"/browse/tvshows"}>
-          <div className="text-white text-xs font-bold flex flex-col items-center justify-center">
+          <div className={`text-white text-xs font-bold flex flex-col items-center justify-center rounded-lg ${
+          location.pathname === "/browse/tvshows" ? "bg-red-500" : ""
+        }`}>
             <i className="ri-tv-2-line "></i>
             {lang[langKey].tvShows}
           </div>
         </Link>
         <Link to={"/browse/peoples"}>
           {" "}
-          <div className="text-white text-xs font-bold flex flex-col items-center justify-center">
+          <div className={`text-white text-xs font-bold flex flex-col items-center justify-center rounded-lg ${
+          location.pathname === "/browse/peoples" ? "bg-red-500" : ""
+        }`}>
             <i className="ri-team-line "></i>
             {lang[langKey].people}
           </div>
