f502ac4e7b0197b5d91a85e053517566a73c0870
Abhay Sharma <abhysharma5113@gmail.com>
2025-01-21 14:17:59 +0530

    Update Browse.jsx

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 import HeaderBrowse from "./HeaderBrowse";
 
@@ -9,6 +9,7 @@ import Logo from "../../src/file.png";
 
 const Browse = () => {
   const langKey = useSelector((store) => store.config.language);
+  const location  = useLocation()
   return (
     <div className="main grid grid-cols-12 w-full h-[100vh] overflow-y-hidden">
   {/* Left Sidebar */}
@@ -20,31 +21,41 @@ const Browse = () => {
         </div>
       </Link>
       <Link to={"/browse/trending"}>
-        <li className="hover:bg-[#E50000] rounded-md px-6 py-2">
+        <li className={`hover:bg-[#E50000] ${
+          location.pathname === "/browse/trending" ? "bg-[#E50000]" : ""
+        } rounded-md px-6 py-2`}>
           <i className="ri-fire-fill mr-2"></i>
           {lang[langKey].trending}
         </li>
       </Link>
       <Link to={"/browse/popular"}>
-        <li className="hover:bg-[#E50000] rounded-md px-6 py-2">
+        <li className={`hover:bg-[#E50000] ${
+          location.pathname === "/browse/popular" ? "bg-[#E50000]" : ""
+        } rounded-md px-6 py-2`}>
           <i className="ri-bard-fill mr-2"></i>
           {lang[langKey].popular}
         </li>
       </Link>
       <Link to={"/browse/movies"}>
-        <li className="hover:bg-[#E50000] rounded-md px-6 py-2">
+        <li className={`hover:bg-[#E50000] ${
+          location.pathname === "/browse/movies" ? "bg-[#E50000]" : ""
+        } rounded-md px-6 py-2`}>
           <i className="ri-movie-2-fill mr-2"></i>
           {lang[langKey].movies}
         </li>
       </Link>
       <Link to={"/browse/tvshows"}>
-        <li className="hover:bg-[#E50000] rounded-md px-6 py-2">
+        <li className={`hover:bg-[#E50000] ${
+          location.pathname === "/browse/tvshows" ? "bg-[#E50000]" : ""
+        } rounded-md px-6 py-2`}>
           <i className="ri-tv-2-fill mr-2"></i>
           {lang[langKey].tvShows}
         </li>
       </Link>
       <Link to={"/browse/peoples"}>
-        <li className="hover:bg-[#E50000] rounded-md px-6 py-2">
+        <li className={`hover:bg-[#E50000] ${
+          location.pathname === "/browse/peoples" ? "bg-[#E50000]" : ""
+        } rounded-md px-6 py-2`}>
           <i className="ri-team-fill mr-2"></i>
           {lang[langKey].people}
         </li>
