8afa1351570406c178da69cd14d4061e0c48bda1
Abhay Sharma <abhysharma5113@gmail.com>
2025-01-24 10:46:17 +0530

    performance : add lazy loading and loaders

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+
+import { CircleLoader, ClimbingBoxLoader, PacmanLoader } from 'react-spinners'
 
 const Loading = () => {
   return (
-    <div className='bg-black w-full h-full text-white text-2xl'>Loading</div>
+    <div className='flex justify-center items-center w-full h-[100vh] bg-black'>
+      {/* <PacmanLoader size={50} color='#E50000'  speedMultiplier={2}/> */}
+      <CircleLoader size={80} color='#E50000'  speedMultiplier={2}/> /
+      {/* <ClimbingBoxLoader size={50} color='#E50000'  speedMultiplier={2}/> / */}
+      
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,82 +1,143 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { Suspense, lazy } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Browse from "./components/Browse.jsx";
-
-import Error from "./components/Error.jsx";
-import Trending from "./components/Trending.jsx";
-import Popular from "./components/Popular.jsx";
-import Movies from "./components/Movies.jsx";
-import Tvshows from "./components/Tvshows.jsx";
-import Person from "./components/Person.jsx";
 import { Provider } from "react-redux";
 import appStore from "./store/appStore.jsx";
-import Saved from "./components/Saved.jsx";
-import GptPage from "./components/GptPage.jsx";
-import BrowseContent from "./components/BrowseContent.jsx";
-import MovieDetails from "./components/MovieDetails.jsx";
-import TvDetails from "./components/TvDetails.jsx";
-import PersonDetails from "./components/PersonDetails.jsx";
+import Loading from "./components/Loading.jsx";
+
+// const App = lazy(() => import("./App.jsx"));
+const Browse = lazy(() => import("./components/Browse.jsx"));
+const BrowseContent = lazy(() => import("./components/BrowseContent.jsx"));
+const Error = lazy(() => import("./components/Error.jsx"));
+const Trending = lazy(() => import("./components/Trending.jsx"));
+const Popular = lazy(() => import("./components/Popular.jsx"));
+const Movies = lazy(() => import("./components/Movies.jsx"));
+const Tvshows = lazy(() => import("./components/Tvshows.jsx"));
+const Person = lazy(() => import("./components/Person.jsx"));
+const Saved = lazy(() => import("./components/Saved.jsx"));
+const GptPage = lazy(() => import("./components/GptPage.jsx"));
+const MovieDetails = lazy(() => import("./components/MovieDetails.jsx"));
+const TvDetails = lazy(() => import("./components/TvDetails.jsx"));
+const PersonDetails = lazy(() => import("./components/PersonDetails.jsx"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-    errorElement: <Error />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <App />
+      </Suspense>
+    ),
+    errorElement: (
+      <Suspense fallback={<Loading />}>
+        <Error />
+      </Suspense>
+    ),
   },
   {
     path: "/browse",
-    element: <Browse />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <Browse />
+      </Suspense>
+    ),
     children: [
       {
         path: "/browse/trending",
-        element: <Trending />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Trending />
+          </Suspense>
+        ),
       },
       {
         path: "/browse",
-        element: <BrowseContent />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <BrowseContent />
+          </Suspense>
+        ),
       },
-      
       {
         path: "/browse/popular",
-        element: <Popular />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Popular />
+          </Suspense>
+        ),
       },
       {
         path: "/browse/movies",
-        element: <Movies />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Movies />
+          </Suspense>
+        ),
       },
       {
         path: "/browse/tvshows",
-        element: <Tvshows />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Tvshows />
+          </Suspense>
+        ),
       },
       {
         path: "/browse/peoples",
-        element: <Person />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Person />
+          </Suspense>
+        ),
       },
       {
         path: "/browse/saved",
-        element: <Saved />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Saved />
+          </Suspense>
+        ),
       },
       {
         path: "/browse/gpt",
-        element: <GptPage />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <GptPage />
+          </Suspense>
+        ),
       },
     ],
-    errorElement: <Error />,
+    errorElement: (
+      <Suspense fallback={<Loading />}>
+        <Error />
+      </Suspense>
+    ),
   },
   {
     path: "/movie/:id",
-    element: <MovieDetails />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <MovieDetails />
+      </Suspense>
+    ),
   },
   {
     path: "/tv/:id",
-    element: <TvDetails />,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <TvDetails />
+      </Suspense>
+    ),
   },
   {
     path: "/person/:id",
-    element: <PersonDetails />,
+    element: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <PersonDetails />
+      </Suspense>
+    ),
   },
-  
 ]);
 
 createRoot(document.getElementById("root")).render(
